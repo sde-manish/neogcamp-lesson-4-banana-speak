29e956258bf258aa580e48c72de669de5b24b8d8
Manish Kumar <manish481181@gmail.com>
2022-09-28 23:33:38 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+/*What is vanilla JavaScript?
+Vanilla JavaScript refers to using plain Javascript without any additional
+ libraries or frameworks. The term became popular when Eric Wastl created 
+ the Vanilla JS site in 2012 as a joke. The site tries to bring attention 
+ to the fact that you can use just plain Javascript in many cases.*/
+
+// console.log("script is working from a different file")
+
+// alert("this script works")  {this is one way to show alert message}
+
+// var username=prompt("give me your username")
+// alert("this script works "+username);
+
+// input
+var username =prompt("give me username")
+// processing
+var welcomemessage ="this script works "+username;
+// output
+alert(welcomemessage)
